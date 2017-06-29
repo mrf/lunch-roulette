ea0386ba886cf653994630362973d7af266cec4c
Mark Ferree <markferree@gmail.com>
2017-06-29 13:16:30 -0700

    Modularizing code

--- listing.py
+++ listing.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
 
 from __future__ import print_function
-
 import boto3
 
-client = boto3.client('dynamodb')
-
-response = client.scan(TableName="People")
-
-print(response)
+def full_table():
+    client = boto3.client('dynamodb')
+    response = client.scan(TableName="People")
+    return response
