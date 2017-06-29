59788b6d936c81d41f5e0bde134cc785715b5a98
Mark Ferree <markferree@gmail.com>
2017-06-29 10:56:49 -0700

    Landing page

---
+++ index.py
@@ -0,0 +1,17 @@
+#!/bin/python2
+
+from __future__ import print_function
+
+# Set our document type so that we can render it (python is weird)
+print("Content-Type: text/plain;charset=utf-8")
+
+
+# TODO: Multiline print would look cleaner
+print("<html>")
+print("  <head>")
+print('    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">')
+print("  </head>")
+print("  <body>")
+print("     <h1>Welcome to Lunch Roulette!</h1>")
+print("  </body>")
+print("</html>")
