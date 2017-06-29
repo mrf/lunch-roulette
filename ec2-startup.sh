eac7e341389a6096e7d5ed3384c8efb390a77f13
Mark Ferree <markferree@gmail.com>
2017-06-29 12:04:24 -0700

    Tweaks from http conf and making executable

--- ec2-startup.sh
+++ ec2-startup.sh
@@ -9,9 +9,9 @@ yum install httpd git -y
 # Write basic python config for apache
 echo "<Directory /var/www/html/lunch-roulette>" >> /etc/httpd/conf.d/python.conf
 echo "    Options +ExecCGI" >> /etc/httpd/conf.d/python.conf
+echo "    AddHandler cgi-script .py" >> /etc/httpd/conf.d/python.conf
 echo "    DirectoryIndex index.py" >> /etc/httpd/conf.d/python.conf
 echo "</Directory>" >> /etc/httpd/conf.d/python.conf
-echo "AddHandler cgi-script .py" >> /etc/httpd/conf.d/python.conf
 
 # Get apache started up and auto-restarted on instance restart
 service httpd start
@@ -20,6 +20,9 @@ chkconfig httpd on
 # Go to our webroot and clone the application code
 cd /var/www/html/
 git clone https://github.com/mrf/lunch-roulette.git
+
+# Set permissions to executable
+chmod +x index.py
 
 # Install Python SDK for AWS so we can connect to dynamo
 pip install boto3
