401fb96d997851b9e1f7d6115fbb21317fe3cfce
Mark Ferree <markferree@gmail.com>
2017-06-29 13:46:16 -0700

    Working listing of people from Dynamo

--- listing.py
+++ listing.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
+"""Functions to return values from Dynamo database"""
 
 from __future__ import print_function
 import boto3
 
 def full_table():
+    """Return all results from Dynamo"""
     client = boto3.client('dynamodb')
     response = client.scan(TableName="People")
-    return response
+    # TODO return as a clean object
+    return response['Items']
