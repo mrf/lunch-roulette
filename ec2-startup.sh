3979c95dbb8e2029ff980f04b5cb3c221bf418b4
Mark Ferree <markferree@gmail.com>
2017-06-28 18:56:44 -0700

    Better comments for startup script

--- ec2-startup.sh
+++ ec2-startup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+# Get everything up to date
 yum update -y
+
+# Get apache + git installed, started up and autostarted on instance restart
 yum install httpd git -y
 service httpd start
 chkconfig httpd on
+
+# Go to our webroot and clone the application code
 cd /var/www/html/
 git clone https://github.com/mrf/lunch-roulette.git
+
+# Install Python SDK for AWS
+pip install boto3
