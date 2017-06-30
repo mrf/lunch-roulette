bf854bdb34dfa43d8439659b4eff869dbde43ffc
Mark Ferree <markferree@gmail.com>
2017-06-29 20:28:41 -0700

    Feedback on successful submission of new user

--- newuser.py
+++ newuser.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-"""Functions to return values from Dynamo database"""
+"""Insert value into Dynamo database"""
 
 from __future__ import print_function
 import os
@@ -7,7 +7,7 @@ import boto3
 
 CLIENT = boto3.client('dynamodb', region_name='us-west-1')
 
-
+# Check that we are executing from a webserver that provided query string
 if os.environ['QUERY_STRING']:
     QUERY = os.environ['QUERY_STRING']
     QUERY_PIECES = QUERY.split('=')
@@ -34,6 +34,12 @@ print('    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstra
 print("  </head>")
 print("  <body>")
 print("     <h1>Please Insert a User</h1>")
-print('     <form action="newuser.py">Name:<input type="text" name="name"></form>')
+print('     <form action="newuser.py">Name:<input type="text" name="name"><input type="submit"></form>')
+# Check for a succesfull submission and report back to user
+if RESPONSE['ResponseMetadata']['HTTPStatusCode'] == 200:
+    print('Inserted user: {}'.format(USER))
+else:
+    print('Failed to insert user for unspecified reason.')
+print('     <a href="index.py">Return to group assignment page</a>')
 print("  </body>")
 print("</html>")
