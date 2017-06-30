9c936597a72efc0efa18f1cc26f25e6388cc44f4
Mark Ferree <markferree@gmail.com>
2017-06-29 20:12:25 -0700

    New user creation form fixed

--- newuser.py
+++ newuser.py
@@ -2,27 +2,30 @@
 """Functions to return values from Dynamo database"""
 
 from __future__ import print_function
+import os
 import boto3
 
 CLIENT = boto3.client('dynamodb', region_name='us-west-1')
 
-USER = 'Frank'
+QUERY = os.environ['QUERY_STRING']
+QUERY_PIECES = QUERY.split('=')
+USER = QUERY_PIECES[1]
 
-RESPONSE = CLIENT.put_item(
-    TableName="People",
-    Item={
-        'name': {
-            'S': USER,
-        }
-    },
-    ReturnConsumedCapacity='TOTAL',
-)
-
-print(RESPONSE)
+# Prevent bots filling form with junk
+if QUERY_PIECES[0] == 'name':
+    # Send our user query to Dynamo
+    RESPONSE = CLIENT.put_item(
+        TableName="People",
+        Item={
+            'name': {
+                'S': USER,
+            }
+        },
+        ReturnConsumedCapacity='TOTAL',
+    )
 
 # Set our document type so that CGI can render it
 print("Content-Type: text/html\n\n")
-
 print("<html>")
 print("  <head>")
 print('    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">')
